d02f90d2166f65604cadae31f75473bde7f1bac7
evaabsalyamova <absalyamovaeva@gmail.com>
2023-10-30 19:44:52 +0400

    Add choosing symbol feature

--- src/components/CalcBlock/index.tsx
+++ src/components/CalcBlock/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./styles.css";
+import OptionBlock from "../OptionBlock";
 
 const CalcBlock: React.FunctionComponent = () => {
   const [number, setNumber] = useState<string>();
   const [number2, setNumber2] = useState<string>();
   const [result, setResult] = useState<number>();
+  const [option, setOption] = useState<string>("+");
 
   return (
     <div className="containerCalcBlock">
@@ -13,13 +15,30 @@ const CalcBlock: React.FunctionComponent = () => {
         onChange={(enteredNum) => setNumber(enteredNum.target.value)}
         value={number}
       />
-      <div>+</div>
+      <div>
+        <OptionBlock onOptionSelect={setOption} />
+      </div>
       <input
         placeholder="Value"
         onChange={(enteredNum) => setNumber2(enteredNum.target.value)}
         value={number2}
       />
-      <button onClick={() => setResult(Number(number) + Number(number2))}>
+      <button
+        onClick={() => {
+          if (option === "+") {
+            setResult(Number(number) + Number(number2));
+          }
+          if (option === "-") {
+            setResult(Number(number) - Number(number2));
+          }
+          if (option === "*") {
+            setResult(Number(number) * Number(number2));
+          }
+          if (option === "/") {
+            setResult(Number(number) / Number(number2));
+          }
+        }}
+      >
         =
       </button>
       {result && <div>{result}</div>}
