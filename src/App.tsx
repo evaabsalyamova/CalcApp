7911d80bf73a2f6c0cae0645054a625ea9746468
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-04 18:54:33 +0400

    Use `HistoryBlock` in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CalcBlock from "./components/CalcBlock";
 import "./App.css";
+import HistoryBlock from "./components/HistoryBlock";
 
 function App() {
   return (
     <div className="App">
-      <span className="appTitle">Your calc</span>
+      <span>Your calc</span>
       <div className="container">
+        <HistoryBlock />
         <CalcBlock />
       </div>
     </div>
