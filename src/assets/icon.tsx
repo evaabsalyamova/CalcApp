690bf59467bcd224367c2132c27bd8a41c9cbcd2
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-04 18:52:16 +0400

    Create `CalcItem` component

---
+++ src/assets/icon.tsx
@@ -0,0 +1,38 @@
+export const icons = {
+  checkReady: (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 0.6 0.6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke="#1C274C"
+        stroke-width=".038"
+        d="M.55.3A.25.25 0 0 1 .3.55.25.25 0 0 1 .05.3a.25.25 0 0 1 .5 0z"
+      />
+      <path
+        d="m.213.313.05.05.125-.125"
+        stroke="#1C274C"
+        stroke-width=".038"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  ),
+
+  delete: (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 0.8 0.8"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M.4.75a.35.35 0 1 1 0-.7.35.35 0 0 1 0 .7ZM.4 0a.4.4 0 1 0 0 .8.4.4 0 0 0 0-.8Zm.143.257a.025.025 0 0 0-.036 0L.4.364.294.258a.025.025 0 0 0-.035 0 .025.025 0 0 0 0 .035l.106.106-.107.107a.025.025 0 0 0 0 .036c.01.01.026.01.036 0L.401.435l.106.106A.025.025 0 1 0 .542.506L.436.4.543.293a.025.025 0 0 0 0-.036Z"
+        fill-rule="evenodd"
+      />
+    </svg>
+  ),
+};
---
+++ src/components/CalcItem/index.tsx
@@ -0,0 +1,23 @@
+import { icons } from "../../assets/icon";
+import { ICalc } from "../../redux";
+import "./styles.css";
+
+interface IProps {
+  calc: ICalc;
+}
+
+const CalcItem: React.FunctionComponent<IProps> = ({ calc }) => {
+  return (
+    <>
+      <div className="calcHeader">
+        <div className="calcTitle">{`${calc.value1} ${calc.symbol} ${calc.value2} = ${calc.result}`}</div>
+        <div className="calcButtons">
+          <div>{icons.checkReady}</div>
+          <div>{icons.delete}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CalcItem;
