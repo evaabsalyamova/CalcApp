a1b719f57816c139d41c2e3c8d3aaaf027c571c5
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-08 14:37:27 +0400

    Wrap negative value to brackets

--- src/components/CalcItem/index.tsx
+++ src/components/CalcItem/index.tsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { icons } from "../../assets/icon";
 import { ICalc, deleteCalc } from "../../redux";
 import "./styles.css";
+import { useMemo } from "react";
 
 interface IProps {
   calc: ICalc;
@@ -14,11 +15,15 @@ const CalcItem: React.FunctionComponent<IProps> = ({ calc }) => {
     dispatch(deleteCalc({ id: calc.id }));
   };
 
+  const value2 = useMemo(() => {
+    return calc.value2[0] === "-" ? `(${calc.value2})` : calc.value2;
+  }, [calc.value2]);
+
   return (
     <>
       <div className="calcHeader">
         <div className="calcTitle">
-          {`${calc.value1} ${calc.symbol} ${calc.value2} = ${calc.result}`}
+          {`${calc.value1} ${calc.symbol} ${value2} = ${calc.result}`}
         </div>
         <div className="calcButtons">
           <div>{icons.showResult}</div>
