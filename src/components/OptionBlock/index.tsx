cf9b57514714bbd7ef0b7f42c7808ebf6a4ec020
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-09 19:22:26 +0400

    Add styles for `OptionBlock`

--- src/components/OptionBlock/index.tsx
+++ src/components/OptionBlock/index.tsx
@@ -1,10 +1,16 @@
+import "./styles.css";
+
 interface IProps {
   onOptionSelect: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const OptionBlock: React.FunctionComponent<IProps> = ({ onOptionSelect }) => {
   return (
-    <select name="action" onChange={(e) => onOptionSelect(e.target.value)}>
+    <select
+      className="selectOption"
+      name="action"
+      onChange={(e) => onOptionSelect(e.target.value)}
+    >
       <option value="+">+</option>
       <option value="-">-</option>
       <option value="*">*</option>
